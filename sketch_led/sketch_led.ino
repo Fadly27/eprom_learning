032693f80536a7abea1b29c7b159fce71e3fd79c
Fadly27 <muhnurulfadli3@gmail.com>
2022-02-19 13:15:40 +0700

    menambaahkan peogram arduino

---
+++ sketch_led/sketch_led.ino
@@ -0,0 +1,42 @@
+int led = 13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT);
+for(int i=1; i<=5; i++){
+  
+  if(i<=5){
+    digitalWrite(led,HIGH);
+    delay(1000);
+    digitalWrite(led,LOW);
+    delay(1000);
+    }
+}
+
+//digitalWrite(led,HIGH);
+//delay(1000);
+//digitalWrite(led,LOW);
+//delay(1000);
+//digitalWrite(led,HIGH);
+//delay(1000);
+//digitalWrite(led,LOW);
+//delay(1000);
+//digitalWrite(led,HIGH);
+//delay(1000);
+//digitalWrite(led,LOW);
+//delay(1000);
+//digitalWrite(led,HIGH);
+//delay(1000);
+//digitalWrite(led,LOW);
+//delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//for(int i=0,i++);{
+//  digitalWrite(led,HIGH);
+//  delay(1000);
+//  digitalWrite(led,LOW);
+//  delay(1000);
+//}
+
+}
